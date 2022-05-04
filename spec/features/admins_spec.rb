427cb0e228be8f7768a619e6c88423bb05ee698b
GoodVen <greentet777@gmail.com>
2022-05-04 20:48:08 +0200

    Add specs for number of kudos

--- spec/features/admins_spec.rb
+++ spec/features/admins_spec.rb
@@ -1,24 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Admins', type: :feature do
-  context 'log in as admin' do
+  @admin = Admin.new(email:"admin1@test.com", password:'111111')
+  @admin.save
+  @employee1 = Employee.create!(email:"test1@test.com", password:'111111')
+  @employee2 = Employee.create!(email:"test2@test.com", password:'111111')
+  @kudo = Kudo.create!(title: '1', content: 'donef', giver: Employee.find_by(email: 'test1@test.com'), receiver: Employee.find_by(email: 'test2@test.com'))
+
+  
+  context 'log in as admin and RD kudo' do
     it 'is succesfull' do
       visit 'http://localhost:3000'
 
       click_link 'Sign in as Admin'
 
       within('form') do
-        fill_in 'Email', with: 'adminone@test.com'
-        fill_in 'Password', with: 'password'
+        fill_in 'Email', with: 'admin1@test.com'
+        fill_in 'Password', with: '111111'
       end
       click_button 'Log in'
       expect(page).to have_content('Congratulate you are admin')
       click_link 'Kudos'
       expect(page).to have_content('Kudos')
-      find('tr', text: '8').click_link('Show')
+      click_link 'Show'
       expect(page).to have_content('Title')
       click_link 'Destroy'
-      expect(page).to have_content('Kudos')
+      expect(page).to have_content('Kudo was successfully destroyed.')
       click_link 'Back'
 
       click_link 'Sign out'
--- spec/features/kudos_spec.rb
+++ spec/features/kudos_spec.rb
@@ -3,43 +3,64 @@ require 'rails_helper'
 RSpec.describe 'Kudos', type: :feature do
   context 'create new employee and create, update, delete new kudo' do
     it 'is succesfull' do
-      visit 'http://localhost:3000/employees/sign_up'
+      visit 'http://localhost:3000/employee/sign_up'
       within('form') do
         fill_in 'Email', with: 'testo5@test.com'
         fill_in 'Password', with: '123456'
         fill_in 'Password confirmation', with: '123456'
       end
       click_button 'Sign up'
-      expect(page).to have_content('Welcome! You have signed up successfully.')
+      expect(page).to have_content('Employee was successfully created.')
 
-      click_link 'New Kudo'
-      expect(page).to have_content('Navigation')
+      visit 'http://localhost:3000/employee/sign_up'
       within('form') do
-        fill_in 'Title', with: 'some title'
+        fill_in 'Email', with: 'testo4@test.com'
+        fill_in 'Password', with: '123456'
+        fill_in 'Password confirmation', with: '123456'
+      end
+      click_button 'Sign up'
+      expect(page).to have_content('Employee was successfully created.')
+
+      visit 'http://localhost:3000/employee/sign_in'
+      within('form') do
+        fill_in 'Email', with: 'testo5@test.com'
+        fill_in 'Password', with: '123456'
+      end
+      click_button 'Log in'
+      expect(page).to have_content('You signed in as testo5@test.com')
+      expect(page).to have_content('Max Kudos available:10')
+
+      click_link 'Kudos'
+      expect(page).to have_content('Number of available Kudos = 10')
+      click_link 'New Kudo'
+      
+      within('form') do
+        fill_in 'Title', with: '1'
         fill_in 'Content', with: 'Hi employee'
-        # select "employee1@test.com"
+        select "testo4@test.com"
       end
       click_button 'Create Kudo'
       expect(page).to have_content('Kudo was successfully created.')
+      expect(page).to have_content('Number of available Kudos = 9')
 
-      find('tr', text: 'some title').click_link('Edit')
-      # click_link 'Edit'
+      find('tr', text: '1').click_link('Edit')
       expect(page).to have_content('Editing Kudo')
       within('form') do
-        fill_in 'Title', with: 'some title!'
+        fill_in 'Title', with: '2!'
         fill_in 'Content', with: 'Hi employee, hi'
-        # select "employee1@test.com"
       end
       click_button 'Update Kudo'
       expect(page).to have_content('Kudo was successfully updated.')
       click_link 'Back'
       expect(page).to have_content('Kudos')
 
-      find('tr', text: 'some title!').click_link('Destroy')
+      find('tr', text: '2!').click_link('Destroy')
       expect(page).to have_content('Kudo was successfully destroyed.')
+      expect(page).to have_content('Number of available Kudos = 10')
+      click_link 'Back'
 
       click_link 'Sign out'
-      expect(page).to have_content('You need to sign in or sign up before continuing.')
+      expect(page).to have_content('Signed out successfully.')
     end
   end
 end
